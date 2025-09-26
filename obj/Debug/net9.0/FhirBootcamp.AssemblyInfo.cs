99d50a13965bea6352057965113e9ca51efc97ae
Gerald Ncube <gncube@gmail.com>
2025-09-26 13:02:19 +0100

    Remove sample weather data JSON file from the project

--- obj/Debug/net9.0/FhirBootcamp.AssemblyInfo.cs
+++ obj/Debug/net9.0/FhirBootcamp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FhirBootcamp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fc8acbe7b35bd961dacefc4a1b03c28520d70d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f5297f2354e1560b5cbc4a1d17cce61329ab5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FhirBootcamp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FhirBootcamp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
